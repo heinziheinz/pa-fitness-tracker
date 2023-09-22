23568eec9ba52e4e9900fed987e5007e9c533857
johnnyRobot <beitschalbua@gmx.at>
2023-09-22 14:40:42 +0200

    save

--- src/main/java/service/FitnessTracker.java
+++ src/main/java/service/FitnessTracker.java
@@ -9,17 +9,11 @@ import java.util.List;
 public class FitnessTracker {
 
     public List<Workout> allWorkouts(User user, WorkoutType workoutType){
-        //   - Can you create a function that retrieves all workouts of a
-        //   certain type that were completed by a certain user by username?
-
         return user.workoutList().stream().filter(workout -> workout.workoutType() == workoutType)
                 .filter(workout -> workout.goal().completed()).toList();
     }
 
     public List<Workout> allWorkoutsHaventBennCompleted(List<User> users, int duration){
-        //   - Can you create a function that retrieves all workouts
-        //   from all users that have at least a minimum duration and haven't been completed?
-
         return users.stream().flatMap(user -> user.workoutList().stream())
                 .filter(workout -> workout.goal().completed() && workout.minutes() > duration).toList();
     }
--- src/test/java/service/FitnessTrackerTest.java
+++ src/test/java/service/FitnessTrackerTest.java
@@ -14,19 +14,22 @@ class FitnessTrackerTest {
     void allWorkouts() {
         Goal goalONe = new Goal(1, GoalType.MUSCLEGAIN, true);
         Workout workoutOne = new Workout(1, WorkoutType.STRENGTH, 30, goalONe);
-        Goal goalTwo = new Goal(1, GoalType.WEIGHTLOSS, false);
+        Goal goalTwo = new Goal(2, GoalType.WEIGHTLOSS, false);
         Workout workoutTwo = new Workout(1, WorkoutType.STRENGTH, 30, goalTwo);
         List<Workout> workouList = List.of(workoutOne, workoutTwo);
         User user = new User(1, "Karl", "alsd#", "as@fmg.at", workouList);
         FitnessTracker fitnessTracker = new FitnessTracker();
         List<Workout> actual = fitnessTracker.allWorkouts(user, WorkoutType.STRENGTH);
-        List<Workout> expected = List.of(workoutOne, workoutTwo);
+        List<Workout> expected = List.of(workoutOne);
+        System.out.println( "actual: " + actual);
+        System.out.println( "expected: " + expected);
         Assertions.assertEquals(expected, actual);
 
     }
 
     @Test
     void allWorkoutsHavenNotBennCompleted() {
+        // test need to be adopted!!
         //   - Can you create a function that retrieves
         //   all workouts from all users that have at least a minimum duration and haven't been completed?
         Workout workout = new Workout(1,
